5550d3992ddffc860f83eee3832947338de26404
Vlad Niculae <vlad@vene.ro>
2019-02-13 15:24:33 +0000

    start root finding from scratch, secant is broken

--- bench.py
+++ bench.py
@@ -4,7 +4,7 @@ import torch
 
 from onmt.modules.sparse_activations import Sparsemax, Tsallis15
 from onmt.modules.sparse_losses import SparsemaxLoss, Tsallis15Loss
-from onmt.modules.root_finding import TsallisSecant
+from onmt.modules.root_finding import TsallisSecant, SparsemaxSecant
 
 
 def bench(f):
@@ -21,25 +21,21 @@ def bench(f):
 if __name__ == '__main__':
 
     ignore_index = 1
-    # y = torch.load('test_target.pt')
 
     import sys
 
-    location = None
+    # location = None
+    location = 'cuda'
     if len(sys.argv) > 1:
         location = sys.argv[1]
 
     x = torch.load('test_input.pt', map_location=location)
+    # x = torch.randn(1280, 35820, device=location)
 
+    # x = x[:1, :100000] # .to(dtype=torch.float64)
     # x.detach_()
-    # y.detach_()
 
-    # x = x[:128]
-    # y = y[:128]
-
-    # x = x[:, :2000]
-    print("x shape", x.shape, x.device, x.dtype)
-    # print("y shape", y.shape, y.device)
+    print("x", x.shape, x.device, x.dtype)
 
     def _sort():
         return torch.sort(x, dim=-1)
@@ -56,11 +52,15 @@ if __name__ == '__main__':
     def _tsallis():
         return Tsallis15(dim=-1)(x)
 
-    def _tsallis_secant():
-        return TsallisSecant(alpha=1.5, dim=-1, n_iter=1000, tol=1e-5)(x)
+#    def _tsallis_secant():
+#        return TsallisSecant(alpha=1.5, dim=-1, n_iter=100000, tol=1e-6)(x)
 
-    def _sparsemax_secant():
-        return TsallisSecant(alpha=2, dim=-1, n_iter=1000, tol=1e-5)(x)
+#   specialized method is faster
+#    def _sparsemax_secant2():
+#        return TsallisSecant(alpha=2, dim=-1, n_iter=100, tol=1e-5)(x)
+
+#    def _sparsemax_secant():
+#        return SparsemaxSecant(dim=-1, n_iter=100000, tol=1e-6)(x)
 
     def softmax_loss():
         lsm = torch.nn.LogSoftmax(dim=-1)
@@ -73,7 +73,8 @@ if __name__ == '__main__':
 
         return sp(x, y)
 
-    print(torch.mean((_tsallis_secant() - _tsallis()) ** 2))
+#    print("tsallis accuracy", torch.sum((_tsallis_secant() - _tsallis()) ** 2))
+#    print("sparsemax accuracy", torch.sum((_sparsemax_secant() - _sparsemax()) ** 2))
 
     torch.cuda.synchronize()
     torch.cuda.synchronize()
@@ -87,17 +88,14 @@ if __name__ == '__main__':
     softmax_timings = bench(_softmax)
     print("softmax   ", np.percentile(softmax_timings, [25, 50, 75]))
 
-    secant_timings = bench(_tsallis_secant)
-    print("secant 1.5", np.percentile(secant_timings, [25, 50, 75]))
-
-    secant_timings = bench(_sparsemax_secant)
-    print("secant   2", np.percentile(secant_timings, [25, 50, 75]))
+#     secant_timings = bench(_tsallis_secant)
+#     print("secant 1.5", np.percentile(secant_timings, [25, 50, 75]))
+#
+#     secant_timings = bench(_sparsemax_secant)
+#     print("secant   2", np.percentile(secant_timings, [25, 50, 75]))
 
     sparsemax_timings = bench(_sparsemax)
     print("sparsemax ", np.percentile(sparsemax_timings, [25, 50, 75]))
 
     tsallis_timings = bench(_tsallis)
     print("tsallis15 ", np.percentile(tsallis_timings, [25, 50, 75]))
-
-
-
