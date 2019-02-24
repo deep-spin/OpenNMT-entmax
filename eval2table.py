f4f26f090eaac67528d8be2e33fa6f646015a194
bpopeters <bpeters@coli.uni-saarland.de>
2019-02-24 17:37:51 +0000

    switch order of output and attention, update path parsing

--- eval2table.py
+++ eval2table.py
@@ -8,40 +8,43 @@ from os.path import basename, split
 import re
 
 
-func_names = {'so': 'softmax', 'sp': 'sparsemax', 'ts': '1.5-entmax'}
+func_names = {'so': '1', 'sp': '2', 'ts': '1.5'}
 
 
 def language(path):
     return basename(path).split('.')[0]
 
 
-def attn(path):
-    return func_names[path.split('/')[-3].split('-')[0]]
+#def attn(path):
+#    return func_names[path.split('/')[-3].split('-')[0]]
 
+
+def attn(path):
+    return func_names[next(re.finditer(r'(so|sp|ts)-(so|sp|ts)', path)).group(0).split('-')[0]]
+
+
+#def out(path):
+#    return func_names[path.split('/')[-3].split('-')[1]]
 
 def out(path):
-    return func_names[path.split('/')[-3].split('-')[1]]
+    return func_names[next(re.finditer(r'(so|sp|ts)-(so|sp|ts)', path)).group(0).split('-')[1]]
 
 
-def run(path):
+def morph_run(path):
     return path.split('/')[-2]
+
+
+def mt_run(path):
+    return path.split('/')[-1].split('-')[0]
+
+
+def bleu(line):
+    return float(next(re.finditer(r'[0-9]+\.[0-9]+', line)).group(0))
 
 
 def morph_num(line):
     # for accuracy and levenshtein distance
     return float(line.strip().split()[-1])
-
-
-def postprocess_morph(data_dict):
-    table = pd.DataFrame(data_dict)
-    table['Acc.'] = table['acc'].astype(float)
-    table['Lev. Dist.'] = table['lev'].astype(float)
-    table['language'] = table['path'].apply(language)
-    table['Attention'] = table['path'].apply(attn)
-    table['Output'] = table['path'].apply(out)
-    table['run'] = table['path'].apply(run)
-    
-    return table
 
 
 def results_table(path, task, columns):
@@ -50,22 +53,25 @@ def results_table(path, task, columns):
         for col, line in zip(cycle(columns), f):
             data[col].append(line.strip())
     df = pd.DataFrame(data)
-    df['Attention'] = df['path'].apply(attn)
     df['Output'] = df['path'].apply(out)
-    df['run'] = df['path'].apply(run)
+    df['Attention'] = df['path'].apply(attn)
 
     if task == 'morph':
+        df['run'] = df['path'].apply(morph_run)
         df['Acc.'] = df['acc'].apply(morph_num)
         #df['Lev. Dist.'] = df['lev'].apply(morph_num)
         df['language'] = df['path'].apply(language)
+    else:
+        df['run'] = df['path'].apply(mt_run)
+        df['BLEU'] = df['bleu'].apply(bleu)
     return df
 
 
 def setting_results(df):
     single_runs = df.loc[df['run'] != 'ensemble']
-    singles_avg = single_runs.groupby(['Attention', 'Output'], sort=False).mean()
+    singles_avg = single_runs.groupby(['Output', 'Attention']).mean()
     ensemble_run = df.loc[df['run'] == 'ensemble']
-    ensemble_avg = ensemble_run.groupby(['Attention', 'Output'], sort=False).mean()
+    ensemble_avg = ensemble_run.groupby(['Output', 'Attention']).mean()
     return pd.concat([singles_avg, ensemble_avg], axis=1)
 
 
