770205b8e6da534ec60dcef8d4d7b511472d3382
bpopeters <bpeters@coli.uni-saarland.de>
2019-02-27 11:31:35 +0000

    make this actually usable for support size

--- validate.py
+++ validate.py
@@ -1,21 +1,16 @@
 #!/usr/bin/env python
 
-import configargparse
+import argparse
 
-import os
-import random
 import torch
 import torch.nn as nn
 
 import onmt
-import onmt.opts as opts
 
-from onmt.inputters.inputter import build_dataset_iter, lazily_load_dataset, \
-    _load_fields, make_features
+from onmt.inputters.inputter import load_fields_from_vocab, make_features, \
+    DatasetLazyIter
 from onmt.inputters import PAD_WORD
-from onmt.model_builder import build_model
-from onmt.model_builder import load_test_model
-from onmt.utils.logging import init_logger, logger
+from onmt.model_builder import build_base_model
 
 
 class Validator(object):
@@ -34,7 +29,7 @@ class Validator(object):
         """
         # Set model in validating mode.
         stats = {'support': 0, 'tgt_words': 0, 'attended_pos': 0}
-        # what 
+        # what
         with torch.no_grad():
             for batch in valid_iter:
                 src = make_features(batch, 'src', 'text')
@@ -63,11 +58,10 @@ class Validator(object):
         return stats
 
 
-def build_validator(opt, model, fields):
+def build_validator(model, fields):
     """
 
     Args:
-        opt (:obj:`Namespace`): user options (usually from argument parsing)
         model (:obj:`onmt.models.NMTModel`): the model to train
         fields (dict): dict of fields
         optim (:obj:`onmt.utils.Optimizer`): optimizer used during training
@@ -81,77 +75,19 @@ def build_validator(opt, model, fields):
     return Validator(model, padding_idx)
 
 
-def training_opt_postprocessing(opt, device_id):
-    if opt.word_vec_size != -1:
-        opt.src_word_vec_size = opt.word_vec_size
-        opt.tgt_word_vec_size = opt.word_vec_size
+def build_dataset_iter(datasets, fields, batch_size, use_gpu):
+    device = "cuda" if use_gpu else "cpu"
 
-    if opt.layers != -1:
-        opt.enc_layers = opt.layers
-        opt.dec_layers = opt.layers
-
-    if opt.rnn_size != -1:
-        opt.enc_rnn_size = opt.rnn_size
-        opt.dec_rnn_size = opt.rnn_size
-        if opt.model_type == 'text' and opt.enc_rnn_size != opt.dec_rnn_size:
-            raise AssertionError("""We do not support different encoder and
-                                 decoder rnn sizes for translation now.""")
-
-    opt.brnn = (opt.encoder_type == "brnn")
-
-    if torch.cuda.is_available() and not opt.gpu_ranks:
-        logger.info("WARNING: You have a CUDA device, \
-                    should run with -gpu_ranks")
-
-    if opt.seed > 0:
-        torch.manual_seed(opt.seed)
-        # this one is needed for torchtext random call (shuffled iterator)
-        # in multi gpu it ensures datasets are read in the same order
-        random.seed(opt.seed)
-        # some cudnn methods can be random even after fixing the seed
-        # unless you tell it to be deterministic
-        torch.backends.cudnn.deterministic = True
-
-    if device_id >= 0:
-        torch.cuda.set_device(device_id)
-        if opt.seed > 0:
-            # These ensure same initialization in multi gpu mode
-            torch.cuda.manual_seed(opt.seed)
-
-    return opt
+    batch_size_fn = None
+    return DatasetLazyIter(datasets, fields, batch_size, batch_size_fn,
+                           device, False)
 
 
-def main(opt):
-    nb_gpu = len(opt.gpu_ranks)
+def load_model(checkpoint, fields, k=0, bisect_iter=0, gpu=False):
 
-    device_id = 1 if nb_gpu == 1 else -1
 
-    opt = training_opt_postprocessing(opt, device_id)
-    init_logger(opt.log_file)
-    # Load checkpoint if we resume from a previous training.
-    assert opt.train_from is not None
-    logger.info('Loading checkpoint from %s' % opt.train_from)
-    checkpoint = torch.load(opt.train_from,
-                            map_location=lambda storage, loc: storage)
-
-    # Load default opts values then overwrite it with opts from
-    # the checkpoint. It's usefull in order to re-train a model
-    # after adding a new option (not set in checkpoint)
-    dummy_parser = configargparse.ArgumentParser()
-    opts.model_opts(dummy_parser)
-    default_opt = dummy_parser.parse_known_args([])[0]
-
-    model_opt = default_opt
-    model_opt.__dict__.update(checkpoint['opt'].__dict__)
-
-    # Peek the first dataset to determine the data_type.
-    first_dataset = next(lazily_load_dataset("valid", opt))
-
-    # Load fields generated from preprocess phase.
-    fields = _load_fields(first_dataset, 'text', opt, checkpoint)
-
-    # Build model.
-    model = build_model(model_opt, opt, fields, checkpoint)
+    model_opt = checkpoint['opt']
+    model = build_base_model(model_opt, fields, gpu, checkpoint)
 
     alpha_lookup = {'softmax': 1.0, 'tsallis15': 1.5, 'sparsemax': 2.0}
     gen_alpha = alpha_lookup.get(model_opt.generator_function,
@@ -161,22 +97,22 @@ def main(opt):
     if gen_alpha == 1.0:
         gen_func = nn.LogSoftmax(dim=-1)
     elif gen_alpha == 2.0:
-        if opt.k > 0:
-            gen_func = onmt.modules.sparse_activations.SparsemaxTopK(dim=-1, k=opt.k)
-        elif opt.bisect_iter > 0:
-            gen_func = onmt.modules.sparse_activations.SparsemaxBisect(n_iter=opt.bisect_iter)
+        if k > 0:
+            gen_func = onmt.modules.sparse_activations.SparsemaxTopK(dim=-1, k=k)
+        elif bisect_iter > 0:
+            gen_func = onmt.modules.sparse_activations.SparsemaxBisect(n_iter=bisect_iter)
         else:
             gen_func = onmt.modules.sparse_activations.Sparsemax(dim=-1)
-    elif gen_alpha == 1.5 and opt.bisect_iter == 0:
-        if opt.k > 0:
-            gen_func = onmt.modules.sparse_activations.Tsallis15TopK(dim=-1, k=opt.k)
+    elif gen_alpha == 1.5 and bisect_iter == 0:
+        if k > 0:
+            gen_func = onmt.modules.sparse_activations.Tsallis15TopK(dim=-1, k=k)
         else:
             gen_func = onmt.modules.sparse_activations.Tsallis15(dim=-1)
     else:
         # generic tsallis with bisection
-        assert opt.bisect_iter > 0, "Must use bisection with alpha != 1,1.5,2"
+        assert bisect_iter > 0, "Must use bisection with alpha != 1,1.5,2"
         gen_func = onmt.modules.sparse_activations.TsallisBisect(
-            alpha=gen_alpha, n_iter=opt.bisect_iter)
+            alpha=gen_alpha, n_iter=bisect_iter)
 
     gen_weights = model.generator[0] if \
         isinstance(model.generator, nn.Sequential) else model.generator
@@ -186,37 +122,40 @@ def main(opt):
 
     model.eval()
     model.generator.eval()
-    
-    print(model)
 
-    validator = build_validator(opt, model, fields)
+    return model
 
-    def train_iter_fct(): return build_dataset_iter(
-        lazily_load_dataset("train", opt), fields, opt)
 
-    def valid_iter_fct(): return build_dataset_iter(
-        lazily_load_dataset("valid", opt), fields, opt, is_train=False)
+def main(opt):
+    # Build model.
+    for path in opt.models:
+        checkpoint = torch.load(path, map_location=lambda storage, loc: storage)
+        fields = load_fields_from_vocab(checkpoint['vocab'], 'text')
+        fields = {'src': fields['src'], 'tgt': fields['tgt']}
+        model = load_model(
+            checkpoint, fields, k=opt.k, bisect_iter=opt.bisect_iter, gpu=opt.gpu)
 
-    if len(opt.gpu_ranks):
-        logger.info('Validating on GPU: %s' % opt.gpu_ranks)
-    else:
-        logger.info('Validating on CPU, could be very slow')
+        validator = build_validator(model, fields)
+        print(model.generator)
 
-    valid_stats = validator.validate(valid_iter_fct())
-    print('avg. attended positions/tgt word: {}'.format(valid_stats['attended_pos'] / valid_stats['tgt_words']))
-    print('avg. support size: {}'.format(valid_stats['support'] / valid_stats['tgt_words']))
+        # I hate that this has to load the data twice
+        dataset = torch.load(opt.data + '.' + 'valid' + '.0.pt')
+
+        def valid_iter_fct(): return build_dataset_iter(
+            iter([dataset]), fields, opt.batch_size, opt.gpu)
+
+        valid_stats = validator.validate(valid_iter_fct())
+        # print('avg. attended positions/tgt word: {}'.format(valid_stats['attended_pos'] / valid_stats['tgt_words']))
+        print('avg. support size: {}'.format(valid_stats['support'] / valid_stats['tgt_words']))
 
 
 if __name__ == "__main__":
-    parser = configargparse.ArgumentParser(
-        description='train.py',
-        config_file_parser_class=configargparse.YAMLConfigFileParser,
-        formatter_class=configargparse.ArgumentDefaultsHelpFormatter)
-
-    opts.config_opts(parser)
-    opts.add_md_help_argument(parser)
-    opts.model_opts(parser)
-    opts.train_opts(parser)
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data')
+    parser.add_argument('-gpu', action='store_true')
+    parser.add_argument('-models', nargs='+')
+    parser.add_argument('-batch_size', default=64, type=int)
+    parser.add_argument('-k', default=0, type=int)
+    parser.add_argument('-bisect_iter', default=0, type=int)
     opt = parser.parse_args()
     main(opt)
